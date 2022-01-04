8d6ebb3316629dab52e9489b828f3f44f9da32f9
Mohammed Rakib <mohammedrakib81336@gmail.com>
2022-01-04 12:37:17 +0600

    bug fixed

--- src/components/profile/MyBlogs.js
+++ src/components/profile/MyBlogs.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Helmet from "react-helmet";
+import { Link } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
 import Footer from "../shared/footer/Footer";
 import Header from "../shared/Header/Header";
@@ -19,11 +20,18 @@ const MyBlogs = () => {
       <Header />
       <div className="md:w-9/12 w-11/12 mx-auto py-10">
         <span className="text-2xl font-bold py-2">My Blogs</span>
-        <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 pt-10">
-          {userBlogs.map((blog) => (
-            <MyBlog key={blog._id} blog={blog}></MyBlog>
-          ))}
-        </div>
+        {userBlogs.length ? (
+          <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 pt-10">
+            {userBlogs.map((blog) => (
+              <MyBlog key={blog._id} blog={blog}></MyBlog>
+            ))}
+          </div>
+        ) : (
+          <h2 className="py-10">
+            You haven't write a blog yet!{" "}
+            <Link to="/writeBlog">Write a blog</Link>{" "}
+          </h2>
+        )}
       </div>
       <Footer />
     </section>
